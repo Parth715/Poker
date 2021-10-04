51acfc858bbbebbb96f82013ae0a5b2046ef5aa1
Parth Patel <parth2002715@gmail.com>
2021-10-04 09:06:44 -0400

    Add project files.

---
+++ Poker/Cards.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    class Cards
+    {
+        public string Type { get; set; }
+        public int Number { get; set; }
+        
+        public Cards(string a, int b)
+        {
+            Type = a;
+            Number = b;
+        }
+        public Cards(string a)
+        {
+            Type = a;
+        }
+
+    }
+}
---
+++ Poker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Poker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d2 = new Cards("Diamond", 2);
+            var h2 = new Cards("Heart", 2);
+            var s2 = new Cards("Spade", 2);
+            var c2 = new Cards("Club", 2);
+            var d3 = new Cards("Diamond", 3);
+            var h3 = new Cards("Heart", 3);
+            var s3 = new Cards("Spade", 3);
+            var c3 = new Cards("Club", 3);
+        }
+    }
+}
